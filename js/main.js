48af30e6885208067e89de8da408036f74b45ba6
Clayton Lloyd <clayton.lloyd@frontend.com>
2014-08-14 15:55:29 +0100

    Generic Page
    Updates to the wish list page and creation of a generic page template.
    Along with addressing some CSS issues for the top nav.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-
 // RADIO BUTTONS //
-
 $(document).ready(function() {
 	//Set default items to checked
 	$("input[type=radio]:checked").closest('.radio_label').children('label').addClass('active');
@@ -56,9 +54,7 @@ function checkDisabledRadioBtn() {
 
 
 
-
 // CHECKBOXES //
-
 // Script for adding class to checkbox label when clicked
 function checkAppliedChbox() {			
 	var chboxChoice = $('input[type="checkbox"]');
@@ -102,8 +98,140 @@ function checkDisabledChbox() {
 
 
 // SELECT BOXES //
-
-// Inititate the Custom Select Box
 $(function(){
 	$('select.custom').customSelect();
 });
+
+// CUSTOM DROPDOWN CONTAINERS
+function dropdownContainers() {
+	// If you click anywhere on the screen it closes any open options panels
+	$(document).on('click touchstart', function () {
+		$(".dd_panel").hide("slide", { direction: "up" }, 400);
+		$('.dd_link').removeClass('drop_active');
+	});
+	
+	// Stop the event for closing option panels when you click on the panel or a hyperlink to open a panel
+	$(".dd_panel, .dd_link").on('click touchstart', function (e) {
+		e.stopPropagation();
+	});
+	
+	// Show and Hide the Options Panels
+	$('.dd_link').click(function() {
+		var selected_panel = ('#') + $(this).attr('id') + ('_options');
+		
+		if($(selected_panel).css('display') == 'none'){
+			// Remove Existing Active Styling and Apply to current Item
+			$('.dd_link').removeClass('drop_active');
+			$(this).addClass('drop_active');
+			// Hide exisiting open dropdowns and show selected one
+			$('.dd_panel').hide("slide", { direction: "up" }, 400);
+			$(selected_panel).show("slide", { direction: "up" }, 400);
+			// Set Focus to the close button within the options panel
+			$(selected_panel).children('.dd_close_panel').children('a').focus();
+		} else {
+			$(selected_panel).hide("slide", { direction: "up" }, 400);
+			$('.dd_link').removeClass('drop_active');
+		}
+	});
+	
+	// Close the options panel when you click the link
+	$(".dd_close_panel a").click(function() {
+		// Apply focus to the link associated with the panel
+		$(this).parent().parent().parent().children('a').focus();
+		$(this).parent().parent().parent().children('a').removeClass('drop_active');
+		// Hide the panel when you click close
+		$(".dd_panel").hide();
+	});
+};
+
+
+// GET HEIGHT DIFFERENCE FOR MEGA DROPDOWN
+function get_height_diff() {
+	var viewportHeight = $(window).height();
+	var divHeight = ($('.mega_drop').height() + 50);
+	var height_diff = viewportHeight - divHeight;
+	
+	// Check to see if category container height is bigger than viewport
+	if (divHeight >= viewportHeight) {
+		
+		// If it's bigger than the viewport set the height to 100%, else reset it to initial height
+		if(viewportHeight < initialDivHeight) {
+			divHeight = '100%';
+		} else {
+			divHeight = initialDivHeight
+		}
+	} else {
+		divHeight = parseInt(divHeight) + 'px';
+	}
+	// Apply the Height to the container
+	$(".mega_drop").css('height',divHeight);
+	
+	// Get the Difference in height between viewport and category div container and add it as the bottom difference
+	if (height_diff >= 0) {
+		height_diff = parseInt(height_diff) + 'px';
+	} else {
+		height_diff = '0px';
+	}
+	$(".mega_drop").css('bottom',height_diff);
+}
+	
+
+// MEGA DROPDOWN
+function megaDropDown() {
+	$('.mega_drop_overlay').hide();
+	$('.mega_drop').hide();
+	
+	$('.categories').click(function(){
+		if ($('.mega_drop_overlay').is(':visible')) {
+			$('.mega_drop_overlay').fadeOut();
+			$('.mega_drop').hide("slide", { direction: "up" }, 500);
+			$('body').removeClass('modal_visible');
+			$(this).removeClass('active');
+		} else {
+			$('.mega_drop_overlay').fadeIn(300);
+			$('.mega_drop').show("slide", { direction: "up" }, 500);
+			$('body').addClass('modal_visible');
+			$(this).addClass('active');
+		}
+	});
+
+	$('.close_modal').click(function(){
+		$('.mega_drop_overlay').fadeOut();
+		$('.mega_drop').hide("slide", { direction: "up" }, 500);
+		$('body').removeClass('modal_visible');
+		$('.categories').removeClass('active');
+	});
+};
+
+
+// MINI TOP SEARCH
+function showSearchMini() {
+	// show or hide nav search when user scrolls past hero section
+	$('.nav_search').hide();
+	$(window).scroll(function () {
+		var y = $(this).scrollTop();
+		if (y > 220) {
+			$('.nav_search').show("slide", { direction: "left" }, 300);
+		} else {
+			$('.nav_search').hide("slide", { direction: "left" }, 300);
+		}
+	});	
+}
+
+
+// ESCAPE KEY FUNCTIONS
+function escapeKeyFunc() {
+	// What happens when you push the escape key
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			//hide any visible drop_panels
+			$(".dd_panel").hide("slide", { direction: "up" }, 400);
+			$('.dd_link').removeClass('drop_active');
+			//hide the mega drop menu 
+			$(megaDropOverlay).fadeOut();
+			$(megaDropInner).hide("slide", { direction: "up" }, 500);
+			$('body').removeClass('modal_visible');
+			$('.categories').removeClass('active');	
+		}  
+	});
+}
